dfc09e75b20a69a1307eb5d702985a9dc729844c
meet-0036 <meet.dhameliya@tsttechnology.io>
2025-02-06 17:20:18 +0530

    add task

--- React JS/Projects/Project Management/src/App.jsx
+++ React JS/Projects/Project Management/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import NewProject from './components/NewProject.jsx';
-import NoProjectSelected from './components/NoProjectSelected.jsx';
-import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
-    selectedProjectId: undefined,
-    projects: []
+    selectedProjectId: undefined, // Nothing to add
+    projects: [],
   });
 
   function handleStartAddProject() {
-    setProjectsState(prevState => {
+    setProjectsState((prevState) => {
       return {
         ...prevState,
+        // set Null : for adding a new Project
         selectedProjectId: null,
       };
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const NewProject = {
+        ...projectData,
+        id: Number(new Date()),
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, NewProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
--- React JS/Projects/Project Management/src/components/NoProjectSelected.jsx
+++ React JS/Projects/Project Management/src/components/NoProjectSelected.jsx
@@ -1,11 +1,11 @@
-import noProjectImg from "../assets/no-projects.png";
-import Button from "./Button.jsx";
+import noProjectImage from '../assets/no-projects.png';
+import Button from './Button.jsx';
 
 export default function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
-        src={noProjectImg}
+        src={noProjectImage}
         alt="An empty task list"
         className="w-16 h-16 object-contain mx-auto"
       />
