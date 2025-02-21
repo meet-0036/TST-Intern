4a5d72a320d428be006e8eae9af9430dd7ca9e5b
meet-0036 <meet.dhameliya@tsttechnology.io>
2025-02-21 11:24:07 +0530

    add Put Mehod

--- React JS/Projects/HTTP Request Practice/src/components/AvailablePlaces.jsx
+++ React JS/Projects/HTTP Request Practice/src/components/AvailablePlaces.jsx
@@ -1,24 +1,67 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import Places from './Places.jsx';
+import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
+// const places = JSON.parse(localStorage.getItem("places"));
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
+  // Need to creating block that executes only once.
+  // useEffect - handle preventing infinite loop
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/places")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((resData) => {
+  //       setAvailablePlaces(resData.places);
+  //     });
+  // }, []);
+
+  // Using async/await
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+        setIsFetching(false);
+      }
+    }
+
+    fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
