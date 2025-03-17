eb772c850e3bb4efec5df74b54b7d2ece42d91ae
meet-0036 <meet.dhameliya@tsttechnology.io>
2025-03-17 19:12:07 +0530

    add VALIDATION

--- React JS/Projects/Next Js Practice/app/meals/share/page.js
+++ React JS/Projects/Next Js Practice/app/meals/share/page.js
@@ -1,7 +1,12 @@
-import classes from './page.module.css';
-import ImagePicker from "../../../components/meals/image-picker";
+"use client";
+
+import classes from "./page.module.css";
+import ImagePicker from "@/components/meals/image-picker";
+import { shareMeal } from "@/lib/actions.js";
+import  MealsFormSubmit  from "@/components/meals/meals-form-submit.js";
 
 export default function ShareMealPage() {
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +16,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -41,7 +46,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Meal Image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
--- React JS/Projects/Next Js Practice/lib/meals.js
+++ React JS/Projects/Next Js Practice/lib/meals.js
@@ -1,9 +1,14 @@
+import fs from 'node:fs'; // for access file system
+
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
+
 
 const db = sql('meals.db');
 
 export async function getMeals() {
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
   // it directly pass to error page
   // throw new Error("Failed to fetch meals");
@@ -13,4 +18,37 @@ export async function getMeals() {
 
 export function getMeal(slug) {
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split('.').pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error('Saving image failed!');
+    }
+  });
+
+  meal.image = `/images/${fileName}`;
+
+  db.prepare(`
+    INSERT INTO meals
+      (title, summary, instructions, creator, creator_email, image, slug)
+    VALUES (
+      @title,
+      @summary,
+      @instructions,
+      @creator,
+      @creator_email,
+      @image,
+      @slug
+    )
+  `).run(meal);
 }
