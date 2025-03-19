c321c39240c0aed9d7fd7bc8d27d651dba9bbb4e
meet-0036 <meet.dhameliya@tsttechnology.io>
2025-03-19 12:11:29 +0530

    final comit on page router practice

--- React JS/Projects/Next Js Page Router Practice/pages/index.js
+++ React JS/Projects/Next Js Page Router Practice/pages/index.js
@@ -1,39 +1,31 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+// import { useState, useEffect } from "React";
+
 import MeetupList from "../components/meetups/MeetupList";
 
-import { useState, useEffect } from "React";
+function HomePage(props) {
+  // for check pre-rendering data
+  // const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 5, 12345 Some City",
-    description: "This is a first meetup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 10, 12345 Some City",
-    description: "This is a second meetup!",
-  },
-];
+  // useEffect(() => {
+  //   // send a http request and fetch data
+  //   setLoadedMeetups(DUMMY_MEETUPS);
+  // }, []);
 
-// pre-defined function - it's job for pre-rendering props for this component
-
-//Next Js wait for promise to resolve before rendering the component
-// This code executes on build process
-export async function getStaticProps() {
-  // fetch data from an API
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-
-    revalidate: 1, // sec
-  };
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
 }
 
 // export async function getServerSideProps(context) {
@@ -49,16 +41,34 @@ export async function getStaticProps() {
 //   };
 // }
 
-function HomePage(props) {
-  // for check pre-rendering data
-  // const [loadedMeetups, setLoadedMeetups] = useState([]);
+// pre-defined function - it's job for pre-rendering props for this component
 
-  // useEffect(() => {
-  //   // send a http request and fetch data
-  //   setLoadedMeetups(DUMMY_MEETUPS);
-  // }, []);
+//Next Js wait for promise to resolve before rendering the component
+// This code executes on build process
+export async function getStaticProps() {
+  // fetch data from an API
+  const client = await MongoClient.connect(
+    "mongodb+srv://meetdhameliya:nXICEQmu3bHkkPCr@cluster0.mbngr.mongodb.net/meetups_db?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  const db = client.db();
 
-  return <MeetupList meetups={props.meetups} />;
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
 }
 
 export default HomePage;
