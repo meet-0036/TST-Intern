0f146894925764014086b6fbdcbf31b71b21aa54
meet-0036 <meet.dhameliya@tsttechnology.io>
2025-03-21 18:02:39 +0530

    practice on Routes, form actions, optimization

--- React JS/Projects/React Summary Practice (add Posts)/react-app/src/components/Post.jsx
+++ React JS/Projects/React Summary Practice (add Posts)/react-app/src/components/Post.jsx
@@ -1,14 +1,10 @@
-import { Link } from 'react-router-dom';
-
 import classes from './Post.module.css';
 
-function Post({ id, author, body }) {
+function Post({ author, body }) {
   return (
     <li className={classes.post}>
-      <Link to={id}>
-        <p className={classes.author}>{author}</p>
-        <p className={classes.text}>{body}</p>
-      </Link>
+      <p className={classes.author}>{author}</p>
+      <p className={classes.text}>{body}</p>
     </li>
   );
 }
--- React JS/Projects/React Summary Practice (add Posts)/react-app/src/routes/NewPost.jsx
+++ React JS/Projects/React Summary Practice (add Posts)/react-app/src/routes/NewPost.jsx
@@ -31,13 +31,17 @@ export default NewPost;
 export async function action({request}) {
   const formData = await request.formData();
   const postData = Object.fromEntries(formData); // { body: '...', author: '...' }
-  await fetch('http://localhost:8080/posts', {
+  const res = await fetch('http://localhost:8282/posts', {
     method: 'POST',
     body: JSON.stringify(postData),
     headers: {
       'Content-Type': 'application/json',
     },
   });
+
+  if (!res.ok) {
+    throw new Error("Failed to create a post");
+  } 
 
   return redirect('/');
 }
